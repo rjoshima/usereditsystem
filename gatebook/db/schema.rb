566b667364e3ea0cab00f40d803cb57c6e9fa9c3
ryatajoshima <sojoy8@gmail.com>
2016-09-16 05:45:46 +0900

    deviseがインストール可能な状態にできない。

--- gatebook/db/schema.rb
+++ gatebook/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915190449) do
+ActiveRecord::Schema.define(version: 20160915195941) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "titile"
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
