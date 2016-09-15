913aad3ab988654f62a1160ee66bd9b9f99f62db
ryatajoshima <sojoy8@gmail.com>
2016-09-16 04:31:29 +0900

    scss書き込みした

--- gatebook/db/schema.rb
+++ gatebook/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915172412) do
+ActiveRecord::Schema.define(version: 20160915190449) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "titile"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
