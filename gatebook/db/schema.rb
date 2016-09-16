c8f3d97098aaeaa2f0e15b982d323bab172665a1
ryatajoshima <sojoy8@gmail.com>
2016-09-16 14:22:19 +0900

    データベース調整完了

--- gatebook/db/schema.rb
+++ gatebook/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915234015) do
+ActiveRecord::Schema.define(version: 20160916022333) do
 
   create_table "notes", force: :cascade do |t|
-    t.string   "titile"
-    t.string   "content"
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20160915234015) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "image"
+    t.string   "encrypted_password", default: "", null: false
   end
 
 end
